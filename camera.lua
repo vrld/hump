bf862c9b400b0ba10a4de59203bcfe7b6c42dbe7
Matthias Richter <vrld@vrld.org>
2012-11-01 18:49:15 +0100

    Issue 9: Add camera:zoom[To]()

--- camera.lua
+++ camera.lua
@@ -34,17 +34,7 @@ local function new(x,y, zoom, rot)
 	x,y  = x or love.graphics.getWidth()/2, y or love.graphics.getHeight()/2
 	zoom = zoom or 1
 	rot  = rot or 0
-	return setmetatable({x = x, y = y, zoom = zoom, rot = rot}, camera)
-end
-
-function camera:rotate(phi)
-	self.rot = self.rot + phi
-	return self
-end
-
-function camera:rotation(phi)
-	if phi then self.rot = phi end
-	return self.rot
+	return setmetatable({x = x, y = y, scale = zoom, rot = rot}, camera)
 end
 
 function camera:lookAt(x,y)
@@ -61,10 +51,30 @@ function camera:pos()
 	return self.x, self.y
 end
 
+function camera:rotate(phi)
+	self.rot = self.rot + phi
+	return self
+end
+
+function camera:rotateTo(phi)
+	self.rot = phi
+	return self
+end
+
+function camera:zoom(mul)
+	self.scale = self.scale * mul
+	return self
+end
+
+function camera:zoomTo(zoom)
+	self.scale = zoom
+	return self
+end
+
 function camera:attach()
-	local cx,cy = love.graphics.getWidth()/(2*self.zoom), love.graphics.getHeight()/(2*self.zoom)
+	local cx,cy = love.graphics.getWidth()/(2*self.scale), love.graphics.getHeight()/(2*self.scale)
 	love.graphics.push()
-	love.graphics.scale(self.zoom)
+	love.graphics.scale(self.scale)
 	love.graphics.translate(cx, cy)
 	love.graphics.rotate(self.rot)
 	love.graphics.translate(-self.x, -self.y)
@@ -81,19 +91,19 @@ function camera:draw(func)
 end
 
 function camera:cameraCoords(x,y)
-	-- x,y = ((x,y) - (self.x, self.y)):rotated(self.rot) * self.zoom + center
+	-- x,y = ((x,y) - (self.x, self.y)):rotated(self.rot) * self.scale + center
 	local w,h = love.graphics.getWidth(), love.graphics.getHeight()
 	local c,s = cos(self.rot), sin(self.rot)
 	x,y = x - self.x, y - self.y
 	x,y = c*x - s*y, s*x + c*y
-	return x*self.zoom + w/2, y*self.zoom + h/2
+	return x*self.scale + w/2, y*self.scale + h/2
 end
 
 function camera:worldCoords(x,y)
-	-- x,y = (((x,y) - center) / self.zoom):rotated(-self.rot) + (self.x,self.y)
+	-- x,y = (((x,y) - center) / self.scale):rotated(-self.rot) + (self.x,self.y)
 	local w,h = love.graphics.getWidth(), love.graphics.getHeight()
 	local c,s = cos(-self.rot), sin(-self.rot)
-	x,y = (x - w/2) / self.zoom, (y - h/2) / self.zoom
+	x,y = (x - w/2) / self.scale, (y - h/2) / self.scale
 	x,y = c*x - s*y, s*x + c*y
 	return x+self.x, y+self.y
 end
