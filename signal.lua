8d4e5bb65fff8d8b77623290476f40e79103e493
Matthias Richter <vrld@vrld.org>
2013-02-20 16:54:57 +0100

    Update license header

--- signal.lua
+++ signal.lua
@@ -1,5 +1,5 @@
 --[[
-Copyright (c) 2012 Matthias Richter
+Copyright (c) 2012-2013 Matthias Richter
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
