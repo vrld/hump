4f6c85e5145d94bceaf4294cb81608ba6e308926
Matthias Richter <vrld@vrld.org>
2013-03-31 20:53:53 +0200

    Add timer.tween

--- timer.lua
+++ timer.lua
@@ -30,7 +30,7 @@ Timer.__index = Timer
 local function _nothing_() end
 
 local function new()
-	return setmetatable({functions = {}}, Timer)
+	return setmetatable({functions = {}, tween = Timer.tween}, Timer)
 end
 
 function Timer:update(dt)
@@ -79,16 +79,111 @@ function Timer:clear()
 	self.functions = {}
 end
 
+Timer.tween = setmetatable({
+	-- helper functions
+	out = function(f) -- 'rotates' a function
+		return function(s, ...) return 1 - f(1-s, ...) end
+	end,
+	chain = function(f1, f2) -- concatenates two functions
+		return function(s, ...) return (s < .5 and f1(2*s, ...) or 1 + f2(2*s-1, ...)) * .5 end
+	end,
+
+	-- useful tweening functions
+	linear = function(s) return s end,
+	quad   = function(s) return s*s end,
+	cubic  = function(s) return s*s*s end,
+	quart  = function(s) return s*s*s*s end,
+	quint  = function(s) return s*s*s*s*s end,
+	sine   = function(s) return 1-math.cos(s*math.pi/2) end,
+	expo   = function(s) return 2^(10*(s-1)) end,
+	circ   = function(s) return 1 - math.sqrt(1-s*s) end,
+
+	back = function(s,bounciness)
+		bounciness = bounciness or 1.70158
+		return s*s*((bounciness+1)*s - bounciness)
+	end,
+
+	bounce = function(s) -- magic numbers ahead
+		local a,b = 7.5625, 1/2.75
+		return math.min(a*s^2, a*(s-1.5*b)^2 + .75, a*(s-2.25*b)^2 + .9375, a*(s-2.625*b)^2 + .984375)
+	end,
+
+	elastic = function(s, amp, period)
+		amp, period = amp and math.max(1, amp) or 1, period or .3
+		return (-amp * math.sin(2*math.pi/period * (s-1) - math.asin(1/amp))) * 2^(10*(s-1))
+	end,
+}, {
+
+-- register new tween
+__call = function(tween, self, len, subject, target, method, after, ...)
+	-- recursively collects fields that are defined in both subject and target into a flat list
+	local function tween_collect_payload(subject, target, out)
+		for k,v in pairs(target) do
+			local ref = subject[k]
+			assert(type(v) == type(ref), 'Type mismatch in field "'..k..'".')
+			if type(v) == 'table' then
+				tween_collect_payload(ref, v, out)
+			else
+				local ok, delta = pcall(function() return (v-ref)*1 end)
+				assert(ok, 'Field "'..k..'" does not support arithmetic operations')
+				out[#out+1] = {subject, k, delta}
+			end
+		end
+		return out
+	end
+
+	method = tween[method or 'linear'] -- see __index
+	local payload, t, args = tween_collect_payload(subject, target, {}), 0, {...}
+
+	local last_s = 0
+	return self:do_for(len, function(dt)
+		t = t + dt
+		local s = method(math.min(1, t/len), unpack(args))
+		local ds = s - last_s
+		last_s = s
+		for _, info in ipairs(payload) do
+			local ref, key, delta = unpack(info)
+			ref[key] = ref[key] + delta * ds
+		end
+	end, after)
+end,
+
+-- fetches function and generated compositions for method `key`
+__index = function(tweens, key)
+	if type(key) == 'function' then return key end
+
+	assert(type(key) == 'string', 'Method must be function or string.')
+	if rawget(tweens, key) then return rawget(tweens, key) end
+
+	local function construct(pattern, f)
+		local method = rawget(tweens, key:match(pattern))
+		if method then return f(method) end
+		return nil
+	end
+
+	local out, chain = rawget(tweens,'out'), rawget(tweens,'chain')
+	return construct('^in%-([^-]+)$', function(...) return ... end)
+	       or construct('^out%-([^-]+)$', out)
+	       or construct('^in%-out%-([^-]+)$', function(f) return chain(f, out(f)) end)
+	       or construct('^out%-in%-([^-]+)$', function(f) return chain(out(f), f) end)
+	       or error('Unknown interpolation method: ' .. key)
+end})
+
 -- default timer
 local default = new()
 
 -- the module
 return setmetatable({
-	new          = new,
-	update       = function(...) return default:update(...) end,
-	do_for       = function(...) return default:do_for(...) end,
-	add          = function(...) return default:add(...) end,
-	addPeriodic  = function(...) return default:addPeriodic(...) end,
-	cancel       = function(...) return default:cancel(...) end,
-	clear        = function(...) return default:clear(...) end,
+	new         = new,
+	update      = function(...) return default:update(...) end,
+	do_for      = function(...) return default:do_for(...) end,
+	add         = function(...) return default:add(...) end,
+	addPeriodic = function(...) return default:addPeriodic(...) end,
+	cancel      = function(...) return default:cancel(...) end,
+	clear       = function(...) return default:clear(...) end,
+	tween       = setmetatable({}, {
+		__index    = Timer.tween,
+		__newindex = function(_,k,v) Timer.tween[k] = v end,
+		__call     = function(t,...) return default:tween(...) end,
+	})
 }, {__call = new})
