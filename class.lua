dbf60e3dc5bf4192dd5a3a9858544a1f14043521
Nico Prins <nico@nicoprins.com>
2015-05-05 09:26:52 -0700

    Fixed include helper skipping falsy values.

--- class.lua
+++ class.lua
@@ -36,7 +36,7 @@ local function include_helper(to, from, seen)
 	seen[from] = to
 	for k,v in pairs(from) do
 		k = include_helper({}, k, seen) -- keys might also be tables
-		if not to[k] then
+		if to[k] == nil then
 			to[k] = include_helper({}, v, seen)
 		end
 	end
