b6c04082b02e62c6ebde18dcc63061311a7cdaae
grunkgrunk <danielgrunkin@gmail.com>
2017-11-03 22:04:27 +0100

    changed signature of random and added documentation

--- vector-light.lua
+++ vector-light.lua
@@ -83,8 +83,18 @@ local function fromPolar(angle, radius)
 	return cos(angle)*radius, sin(angle)*radius
 end
 
-local function random()
-	return fromPolar(math.random()*2*math.pi)
+local function randomDirection(len_min, len_max)
+	len_min = len_min or 1
+	len_max = len_max or len_min
+
+	assert(len_max > 0, "randomDirection: len_max must be greater than zero")
+	assert(len_max >= len_min, "randomDirection: len_max must be greater than or equal to len_min")
+
+	local range = len_max - len_min
+	local rnd = math.random() * range
+
+	local x,y = fromPolar(math.random()*2*math.pi)
+	return x * (rnd + len_min), y * (rnd + len_min)
 end
 
 local function toPolar(x, y)
@@ -144,9 +154,9 @@ end
 return {
 	str = str,
 
-	fromPolar = fromPolar,
-	toPolar   = toPolar,
-	random    = random,
+	fromPolar       = fromPolar,
+	toPolar         = toPolar,
+	randomDirection = randomDirection,
 
 	-- arithmetic
 	mul    = mul,
--- vector.lua
+++ vector.lua
@@ -40,8 +40,16 @@ local function fromPolar(angle, radius)
 	return new(cos(angle) * radius, sin(angle) * radius)
 end
 
-local function random()
-	return fromPolar(math.random()*2*math.pi)
+local function randomDirection(len_min, len_max)
+	len_min = len_min or 1
+	len_max = len_max or len_min
+
+	assert(len_max > 0, "randomDirection: len_max must be greater than zero")
+	assert(len_max >= len_min, "randomDirection: len_max must be greater than or equal to len_min")
+
+	local range = len_max - len_min
+	local rnd = math.random() * range
+	return fromPolar(math.random()*2*math.pi) * (rnd + len_min)
 end
 
 local function isvector(v)
@@ -201,11 +209,11 @@ end
 
 -- the module
 return setmetatable({
-	new       = new,
-	fromPolar = fromPolar,
-	random    = random,
-	isvector  = isvector,
-	zero      = zero
+	new             = new,
+	fromPolar       = fromPolar,
+	randomDirection = randomDirection,
+	isvector        = isvector,
+	zero            = zero
 }, {
 	__call = function(_, ...) return new(...) end
 })
